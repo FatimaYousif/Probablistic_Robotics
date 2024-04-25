0835aa9ed28aa881a26c14e0c293562fd870e740
Fatima Yousif <49322171+FatimaYousif@users.noreply.github.com>
2024-04-25 13:14:11 +0200

    Add files via upload

---
+++ EKF_Map_based_Localization/DR_3DOFDifferentialDrive.py
@@ -0,0 +1,98 @@
+from Localization import *
+import numpy as np
+from DifferentialDriveSimulatedRobot import *
+
+class DR_3DOFDifferentialDrive(Localization):
+    """
+    Dead Reckoning Localization for a Differential Drive Mobile Robot.
+    """
+    def __init__(self, index, kSteps, robot, x0, *args):
+        """
+        Constructor of the :class:`prlab.DR_3DOFDifferentialDrive` class.
+
+        :param args: Rest of arguments to be passed to the parent constructor
+        """
+
+        super().__init__(index, kSteps, robot, x0, *args)  # call parent constructor
+
+        self.dt = 0.1  # dt is the sampling time at which we iterate the DR
+        self.t_1 = 0.0  # t_1 is the previous time at which we iterated the DR
+        self.wheelRadius = 0.1  # wheel radius
+        self.wheelBase = 0.5  # wheel base
+        self.robot.pulse_x_wheelTurns = 4096  # number of pulses per wheel turn
+
+         # Compute Kn_inv matrix
+        self.Kn_inv = self.robot.pulse_x_wheelTurns*self.dt/(2*np.pi*self.wheelRadius) * np.array([[1, -self.wheelBase/2], [1, self.wheelBase/2]])
+
+    def Localize(self, xk_1, uk):  # motion model
+        """
+        Motion model for the 3DOF (:math:`x_k=[x_{k}~y_{k}~\psi_{k}]^T`) Differential Drive Mobile robot using as input the readings of the wheel encoders (:math:`u_k=[n_L~n_R]^T`).
+
+        :parameter xk_1: previous robot pose estimate (:math:`x_{k-1}=[x_{k-1}~y_{k-1}~\psi_{k-1}]^T`)
+        :parameter uk: input vector (:math:`u_k=[u_{k}~v_{k}~w_{k}~r_{k}]^T`)
+        :return xk: current robot pose estimate (:math:`x_k=[x_{k}~y_{k}~\psi_{k}]^T`)
+        """
+
+        # TODO: to be completed by the student
+
+        dl = uk[0] * 2 * np.pi * self.wheelRadius / self.robot.pulse_x_wheelTurns
+        dr = uk[1] * 2 * np.pi * self.wheelRadius / self.robot.pulse_x_wheelTurns
+
+        #avg
+
+        # ppt
+        d = (dr + dl) / 2
+
+       
+        # xk_1= prev yaw (orientation)
+        # theta=new angle after motion
+        
+        theta = xk_1[2, 0] + np.arctan2((dr - dl), self.wheelBase) 
+        
+        #new pose(xk) -> prev pose(xk_1)
+        # ppt
+        xk = (np.array([
+            [xk_1[0, 0] + d * np.cos(theta)],  #x = prev + dcosθ
+            [xk_1[1, 0] + d * np.sin(theta)],  #y = prev+ dsinθ
+            [theta] #θ
+            ]))
+        return xk
+
+        pass
+
+    def GetInput(self):
+        """
+        Get the input for the motion model. In this case, the input is the readings from both wheel encoders.
+
+        :return: uk:  input vector (:math:`u_k=[n_L~n_R]^T`)
+        """
+
+        # TODO: to be completed by the student
+
+        
+        return self.robot.ReadEncoders()
+
+        # pass
+
+if __name__ == "__main__":
+
+    # feature map. Position of 2 point features in the world frame.
+    M = [CartesianFeature(np.array([[-40, 5]]).T),
+           CartesianFeature(np.array([[-5, 40]]).T),
+           CartesianFeature(np.array([[-5, 25]]).T),
+           CartesianFeature(np.array([[-3, 50]]).T),
+           CartesianFeature(np.array([[-20, 3]]).T),
+           CartesianFeature(np.array([[40,-40]]).T)]  # feature map. Position of 2 point features in the world frame.
+
+    xs0=np.zeros((6,1))   # initial simulated robot pose
+    robot = DifferentialDriveSimulatedRobot(xs0, M) # instantiate the simulated robot object
+
+    kSteps = 5000 # number of simulation steps
+    xsk_1 = xs0 = np.zeros((6, 1))  # initial simulated robot pose
+    index = [IndexStruct("x", 0, None), IndexStruct("y", 1, None), IndexStruct("yaw", 2, 1)] # index of the state vector used for plotting
+
+    x0=Pose3D(np.zeros((3,1)))
+    dr_robot=DR_3DOFDifferentialDrive(index,kSteps,robot,x0)
+    dr_robot.LocalizationLoop(x0, np.array([[0.5, 0.03]]).T)
+
+    exit(0)
